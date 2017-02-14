11a12e2a13b7e76d07dc3297acf195cf3b70eb2d
Jeriesha Smith <jerieshasmith@Jerieshas-MacBook-Pro.local>
2017-02-13 20:22:44 -0500

    revised

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -8,19 +8,11 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
         // write your code here
 
-//        Set set = account.entrySet();
-//        Iterator i = set.iterator();
-//        while (i.hasNext()) {
-//            Map.Entry me = (Map.Entry) i.next();
-//            System.out.print(me.getKey() + ": ");
-//            System.out.println(me.getValue());
+//
          ArrayList options = new ArrayList();
-
-         options.add("Checking? ");
-
-         options.add("Savings? ");
 
         System.out.println("Good Morning!");
 
@@ -36,44 +28,69 @@ public class Main {
         account.put("Mary", new Double(300.59) );
         account.put("Johnny", new Double(1500.36));
         account.put("Walter", new Double(2252.39));
+        account.put("Tony", new Double(398));
 
-        System.out.println("Check if Name is in Hashmap: " + account.containsKey("Mary" +"Johnny" +"Walter" ));
-        if (Boolean.valueOf("false"));{
-        System.out.println("Would like to create an account? " + options.get(0) + options.get(1)  );
-            String create = scanner.nextLine();
-            if (create.equalsIgnoreCase("Checking" )){
-                options.add(options);
-                System.out.println("Thank you for choosing to open an account!");
-                System.out.println("1. Check Balance/ 2.Withdraw Funds/ 3.Cancel");
-                String prompt = scanner.nextLine();
-                while(prompt.startsWith("/")){
-                    switch (prompt){
-                        case "/inv":
-                            {
+        System.out.println("Check if Name is in Hashmap: " + account.containsKey(name));
+        if(account.containsKey(name)){
+
+        } else
+        System.out.println("Would like to create an account? ");
+        String create = scanner.nextLine();
+
+        while (true){
+            System.out.println("1.Check Balance:");
+            System.out.println("2. Withdraw Funds: ");
+            System.out.println("3.Cancel: ");
+            System.out.println("4. Remove account:");
+            String text = scanner.nextLine();
+
+            if(text.equals("1")){
+                System.out.println("Your balance is:" + account.get(name) );
+//                String option = scanner.nextLine();
+
+            } else if(text.equals("2")) {
+                System.out.println("Please input amount to withdraw:");
+                String answer = scanner.nextLine();
+                Double to = Double.valueOf(answer);
+                Double me = account.get(name) - to;
+                System.out.println("Please take your cash ");
+                account.put(name, me);
+                System.out.println("Your new balance" + account.get(name));
+
+
+            }
+                else if(text.equals("4")) {
+                System.out.println("Your account has been removed" + account.remove(name));
+//                String been = scanner.nextLine();
+            }
+                else {
+                System.out.println("Thank you have a nice day! ");
+                break;
+
+
+
+
+
+            } while(text.equalsIgnoreCase("3")){
+                System.out.println("What is your name?");
+
+
+
+            }
+
+
+
+
+
+
+
+
+
 
                             }
 
                     }
                 }
-            }
-
-
-
-
-
-
-
-        {
-
-            }
-
-         {
-
-        }
-
-
-
-        }
 
 
 
@@ -82,14 +99,23 @@ public class Main {
 
 
 
-        }
 
 
 
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
 
 ////        double balance = ((Double)account.get("Mary")).doubleValue();
 //      account.put("Mary", new Double(balance + 1000));
