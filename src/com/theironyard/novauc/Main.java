22f4e3a1b3aa42577b9d5e8486d8996017aa3ebf
Jeriesha Smith <jerieshasmith@Jerieshas-MBP.fios-router.home>
2017-02-26 00:40:28 -0500

    Resubmit

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,5 +1,6 @@
 package com.theironyard.novauc;
 
+import javax.naming.InsufficientResourcesException;
 import java.util.*;
 import java.text.NumberFormat;
 import java.math.*;
@@ -12,7 +13,7 @@ public class Main {
         // write your code here
 
 //
-         ArrayList options = new ArrayList();
+        ArrayList options = new ArrayList();
 
         System.out.println("Good Morning!");
 
@@ -25,73 +26,70 @@ public class Main {
         System.out.println(" Welcome," + name);
 
         HashMap<String, Double> account = new HashMap<>();
-        account.put("Mary", new Double(300.59) );
+        account.put("Mary", new Double(300.59));
         account.put("Johnny", new Double(1500.36));
         account.put("Walter", new Double(2252.39));
         account.put("Tony", new Double(398));
 
-        System.out.println("Check if Name is in Hashmap: " + account.containsKey(name));
-        if(account.containsKey(name)){
+        if (account.containsKey(name)) {
+            System.out.println("Welcome! " + account.get(name));
 
-        } else
-        System.out.println("Would like to create an account? ");
-        String create = scanner.nextLine();
-
-        while (true){
-            System.out.println("1.Check Balance:");
-            System.out.println("2. Withdraw Funds: ");
-            System.out.println("3.Cancel: ");
-            System.out.println("4. Remove account:");
-            String text = scanner.nextLine();
-
-            if(text.equals("1")){
-                System.out.println("Your balance is:" + account.get(name) );
-//                String option = scanner.nextLine();
-
-            } else if(text.equals("2")) {
-                System.out.println("Please input amount to withdraw:");
-                String answer = scanner.nextLine();
-                Double to = Double.valueOf(answer);
-                Double me = account.get(name) - to;
-                System.out.println("Please take your cash ");
-                account.put(name, me);
-                System.out.println("Your new balance" + account.get(name));
-
-
-            }
-                else if(text.equals("4")) {
-                System.out.println("Your account has been removed" + account.remove(name));
-//                String been = scanner.nextLine();
-            }
-                else {
-                System.out.println("Thank you have a nice day! ");
-                break;
-
-
-
-
-
-            } while(text.equalsIgnoreCase("3")){
-                System.out.println("What is your name?");
-
-
-
-            }
-
-
-
-
-
-
-
-
-
-
-                            }
-
-                    }
+        } else if (!account.containsKey(name));
+                System.out.println("Account does not exits!");
+                System.out.println("Would you like to add an account?"+ "[yes/no]" );
+                String add = scanner.nextLine();
+                if (add.equalsIgnoreCase("yes")) {
+                    String create = scanner.nextLine();
+                    Double value = account.get(name);
+                    account.put(name,value);
+                    System.out.println("Your account has been created" + account.get(name));
                 }
 
+            while (true) {
+                System.out.println("1.Check Balance:");
+                System.out.println("2. Withdraw Funds: ");
+                System.out.println("3.Cancel: ");
+                System.out.println("4. Remove account:");
+                String text = scanner.nextLine();
+
+                if (text.equals("1")) {
+                    System.out.println("Your balance is:" + account.get(name));
+                    String option = scanner.nextLine();
+
+                } else if (text.equals("2")) {
+                    System.out.println("Please input amount to withdraw:");
+                    String answer = scanner.nextLine();
+                    if(answer.isEmpty()) {
+                        throw new RuntimeException("Insufficient funds");
+                    }
+                    Double to = Double.valueOf(answer);
+                    Double me = account.get(name) - to;
+                    System.out.println("Please take your cash ");
+                    account.put(name, me);
+                    System.out.println("Your new balance" + account.get(name));
+
+
+                } else if (text.equals("4")) {
+                    System.out.println("Your account has been removed");
+                    account.remove(name);
+                    String been = scanner.nextLine();
+                } else {
+                    System.out.println("Thank you have a nice day! ");
+                    break;
+
+
+                }
+                while (text.equalsIgnoreCase("3")) {
+                    System.out.println("What is your name?");
+
+
+                }
+
+
+            }
+
+        }
+    }
 
 
 
